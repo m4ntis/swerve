519f4bd3c60fb400d2189e7ce87d5c199f8e72c8
Mati Shabtay <matishabtay@gmail.com>
2019-01-20 00:39:29 +0200

    command.go: Documentation

--- command.go
+++ command.go
@@ -7,16 +7,30 @@ import (
 
 // Command represents a command of the interactive debugger.
 type Command struct {
-	Name    string
+	// Name is the command's unique name, used to identify the command. Spaces
+	// aren't supported.
+	Name string
+	// Aliases are optional additional names for the commands. Spaces aren't
+	// supported.
 	Aliases []string
 
+	// Run is the main command's method, executed whenever the command is
+	// executed with valid arguments.
 	Run func(p Prompt, args []string)
 
-	Desc  string
+	// Desc is the command's shortest description. Desc is displayed when using
+	// the builtin 'help' command.
+	Desc string
+	// Usage is the command's usage string. If provided, it will be displayed
+	// when using 'help <cmd>'
 	Usage string
-	Help  string
+	// Help is the command's long string. If provided, it will be displayed when
+	// using 'help <cmd>'
+	Help string
 }
 
+// Title returnes the command's title, a formatted string consisting of it's
+// name and aliases (if any).
 func (c *Command) Title() string {
 	if len(c.Aliases) == 0 {
 		return c.Name
