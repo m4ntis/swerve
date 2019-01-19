1b70370c73740e54dfa68a26895c9abaa91b90a8
Mati Shabtay <matishabtay@gmail.com>
2019-01-19 21:34:29 +0200

    shell.go: Implement command hashing

--- shell.go
+++ shell.go
@@ -6,41 +6,60 @@ import (
 )
 
 type Shell struct {
-	Cmds []Command
+	cmds   []Command
+	hashed map[string]Command
+}
+
+func New() *Shell {
+	return &Shell{}
+}
+
+func (s *Shell) Run() {
 }
 
 // Add adds commands to the Shell's command list.
 //
-// Add will panic if a command with an identical name to an existing command is
-// added.
-func (s Shell) Add(cmds ...Command) {
+// Add will panic if a command with a name or alias identical to an existing
+// command is added.
+func (s *Shell) Add(cmds ...Command) {
 	for _, cmd := range cmds {
-		s.sortedAdd(s.Cmds, cmd)
+		s.sortedAdd(s.cmds, cmd)
+		s.hash(cmd)
 	}
 }
 
 // sortedAdd adds a single Command to the Shell's command list in alphabetical
 // name order.
-func (s Shell) sortedAdd(cmd Command) {
+func (s *Shell) sortedAdd(cmd Command) {
 	for i, c := range s.cmds {
-		if c.Name == cmd.Name {
-			panic("You cannot have two commands with identical names")
-		}
-
 		if c.Name > cmd.Name {
-			s.Cmds = append(s.Cmds[:i], append([]Command{cmd}, s.Cmds[i:]...))
+			s.cmds = append(s.cmds[:i], append([]Command{cmd}, s.cmds[i:]...))
 			return
 		}
 	}
 }
 
+// hash adds references to a command by it's name and aliases.
+func (s *Shell) hash(cmd Command) {
+	names := append(cmd.Aliases, name)
+
+	for _, name := range names {
+		_, ok := s.hashed[name]
+		if ok {
+			panic("You cannot have two commands with identical names")
+		}
+
+		s.hashed[name] = cmd
+	}
+}
+
 // help generates an alphabetically sorted, multi-line help string for the
 // Shell's command list, based on their name, aliases and description.
-func (s Shell) help() string {
+func (s *Shell) help() string {
 	help := "The following commands are available:\n"
 
 	longest := s.longestTitleLength()
-	for _, cmd := range s.Cmds {
+	for _, cmd := range s.cmds {
 		help += fmt.Sprintf("    %s %s %s\n",
 			cmd.Title(),
 			strings.Repeat("-", longest-len(title)+1),
@@ -57,7 +76,7 @@ func (s Shell) help() string {
 // This can more efficiently be stored in the shell's context and calculated
 // once on Shell.Add, but that would add the the Shell's context and
 // documentation.
-func (s Shell) longestTitleLength() int {
+func (s *Shell) longestTitleLength() int {
 	longest := 0
 	for _, cmd := range s.cmds {
 		titleLen := len(cmd.Title())
