217cde07aac313e1d959711d2aecd1966be54457
Mati Shabtay <matishabtay@gmail.com>
2019-01-19 18:41:58 +0200

    command.go: Add aliases and Title method to command

--- command.go
+++ command.go
@@ -1,12 +1,26 @@
 package swerve
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Command represents a command of the interactive debugger.
 type Command struct {
-	Name string
+	Name    string
+	Aliases []string
 
 	Run func(args []string)
 
 	Desc  string
 	Usage string
 	Help  string
+}
+
+func (c *Command) Title() string {
+	if len(c.Aliases) == 0 {
+		return c.Name
+	}
+
+	return fmt.Sprintf("%s (alias: %s)", c.Name, strings.Join(c.Aliases, " | "))
 }
