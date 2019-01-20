b0194746d1943d7c9af956917adbf15140a5e8b2
Mati Shabtay <matishabtay@gmail.com>
2019-01-20 21:47:26 +0200

    command.go: Documentation

--- command.go
+++ command.go
@@ -7,23 +7,23 @@ import (
 
 // Command represents a command of the interactive debugger.
 type Command struct {
-	// Name is the command's unique name, used to identify the command. Spaces
-	// aren't supported.
-	Name string
-	// Aliases are optional additional names for the commands. Spaces aren't
+	// Name is the command's unique name, used to identify it. Spaces aren't
 	// supported.
+	Name string
+	// Aliases are optional additional names that can be used to refer to the
+	// command. Spaces aren't supported.
 	Aliases []string
 
-	// Run is the main command's method, executed whenever the command is
-	// executed with valid arguments.
+	// Run is the command's main method, executed when the command is called
+	// with valid arguments.
 	Run func(p Prompt, args []string)
 
-	// ValidateArgs is an optional function to validate a Commands arguments
+	// ValidateArgs is an optional function to validate a command's arguments
 	// before being run.
 	ValidateArgs func(p Prompt, args []string) (ok bool)
 
 	// Desc is the command's shortest description. Desc is displayed when using
-	// the builtin 'help' command.
+	// the builtin 'help' command to display the command list.
 	Desc string
 	// Usage is the command's usage string. If provided, it will be displayed
 	// when using 'help <cmd>'
